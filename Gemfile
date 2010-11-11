64279a529103335df9ace8a799552312108724f4
Brett Richardson <brett.richardson.nz@gmail.com>
2010-11-11 20:21:36 +0100

    Phlat application init

---
+++ Gemfile
@@ -0,0 +1,21 @@
+# BASE
+gem 'rails', '3.0.1'
+gem 'sqlite3-ruby', '1.2.5', :require => 'sqlite3'
+
+
+# AUTHENTICATION
+gem 'authlogic'
+gem 'aegis'
+
+
+group :development do
+	gem 'rspec-rails', '2.0.1'
+end
+
+
+group :test do
+	gem 'rspec', '2.0.1'
+	gem 'webrat', '0.7.1'
+	gem 'spork', '0.8.4'
+	gem 'factory_girl_rails', '1.0'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Phlat::Application.config.secret_token = '2515d048eb5acb7cb7e1147122d459c076ee694d906cd497069c701b599e929e96e44511a21749c057a74a78bcee399fc49ef3c7e8d1f217321dc78615d9b87f'
